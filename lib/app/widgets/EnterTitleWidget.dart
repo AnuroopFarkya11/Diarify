e3687db395e2e649c10af3dde35e9cfad1ea1c31
Bhavik Kothari <kotharibhavik23@gmail.com>
2023-02-20 00:24:35 +0530

    done with final changes

--- lib/app/widgets/EnterTitleWidget.dart
+++ lib/app/widgets/EnterTitleWidget.dart
@@ -10,15 +10,15 @@ class EnterTitleWidget extends  GetView<NotionEditingController>{
   Widget build(BuildContext context) {
     return Container(
       height: 60,
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Color(0xFF58AEFE),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       child: Padding(
         padding: const EdgeInsets.only(left: 10,right: 10,top: 5,bottom: 5),
         child: TextField(
-          controller: controller.title_controller,
-          decoration: InputDecoration(
+          controller: controller.titleController,
+          decoration: const InputDecoration(
             hintText: "Enter Title",
             hintStyle: TextStyle(
               fontSize: 25,
